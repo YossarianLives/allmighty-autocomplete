5e9ca287826872b4a57c500213c045879cab30e4
Rishi Tank <tank.rishi@gmail.com>
2015-12-15 16:31:26 +0000

    Some minor fixes

--- script/autocomplete.js
+++ script/autocomplete.js
@@ -348,18 +348,17 @@ app.filter('highlight', ['$sce',
         return function(input, searchParam) {
             if (typeof input === 'function') return '';
             if (searchParam) {
-                var term = searchParam.text;
                 //Hightlight the words or semiwords that are present in both the search and the suggestion:
                 var escapeRegexp = function(text) {
-                    return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
+                    return searchParam.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
                 };
-                var words = term.replace(/\ +/g, ' ').split(/\ /g);
+                var words = searchParam.replace(/\ +/g, ' ').split(/\ /g);
                 var escapedWords = words.map(escapeRegexp); //Make sure non alphanumeric characters are escaped properly before constructing the regexp
                 escapedWords.forEach(function(escapedWord) {
                     var wordPattern = '(?!<span[^>]*?>)(' + escapedWord + ')(?![^<]*?<\/span>)(?=[^>]*(<|$))'; //Match the escapedWord only if it's not already wrapped within span tags, and it's not part of an html attribute or tag name (from previous insertions of span tags into the input)
                     var wordRegexp = new RegExp(wordPattern, 'gi');
                     input = input.replace(wordRegexp, "<span class=\"highlight\">$1</span>");
-                    input = "<img src=" + searchParam.image + ">" + input;
+                    input = "<img src=" + input.image + ">" + input;
                 });
             }
             return $sce.trustAsHtml(input);
